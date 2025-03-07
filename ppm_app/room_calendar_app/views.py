c3948fe3ed33eea6c38d47e57caea00e45a5153a
GabeSoler <gsoler@gmail.com>
2025-03-07 22:35:41 +0000

    moved some lines down in event occurrence

--- ppm_app/room_calendar_app/views.py
+++ ppm_app/room_calendar_app/views.py
@@ -46,8 +46,6 @@ def event_occurrence_view(request,event_pk):
     """ add an event, add occurrences, show occurrences"""
     template = 'room_calendar_app/dynamic/event.html'
     event = get_object_or_404(Event, pk=event_pk)
-    occurrences = OccurrenceModel.objects.filter(event=event) #filter events to user
-    form = OccurrenceProxyForm()
     if request.htmx:
         #htmx request triggers save and refresh of occurrences and refreshes form with errors
         form = OccurrenceProxyForm(data=request.POST)
@@ -63,6 +61,8 @@ def event_occurrence_view(request,event_pk):
         context = {'form':form,"event":event,"occurrences":occurrences}
         return render(request,form_list_template,context)
     #else display full page
+    occurrences = OccurrenceModel.objects.filter(event=event) #filter events to user
+    form = OccurrenceProxyForm()
     context = {'form':form,"event":event,"occurrences":occurrences}
     return render(request,template,context)
 
