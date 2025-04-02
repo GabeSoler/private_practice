a65c826e731d02a30841a18ad8efbe7dfb1ae428
GabeSoler <gsoler@gmail.com>
2025-04-02 19:44:37 +0100

    elimiated all files from record

--- ppm_app/room_calendar_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RoomCalendarAppConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "room_calendar_app"
--- ppm_app/session_client/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SessionClientConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "session_client"
