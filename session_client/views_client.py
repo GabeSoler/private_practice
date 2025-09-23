23a4d1253138effbf5464efd03516ff3d9b71a26
GabeSoler <gsoler@gmail.com>
2025-09-23 12:03:04 +0100

    separating veiws into session and client eliminated some views I am not using by replacing with htmx

---
+++ session_client/views_client.py
@@ -0,0 +1,142 @@
+
+from django.shortcuts import render,redirect,get_object_or_404
+from django.urls import reverse
+
+from .models import ClientModel
+from django.contrib.auth.decorators import login_required
+from django.http import Http404,HttpResponse
+from .forms import ClientForm,SearchClientForm,ClientFormShort
+from django_htmx.http import retarget,reswap,HttpResponseClientRedirect
+from django.utils import timezone
+from django.contrib import messages
+
+
+# Create your views here.
+
+
+@login_required
+def clients_view(request):
+    """show all clients"""
+    template = 'session_client/lists/client_list.html'
+    clients = ClientModel.objects.filter(user=request.user,active=True).order_by('code')
+    form = ClientFormShort()
+    if request.htmx:
+        form_partial = ClientFormShort(data=request.POST)
+        if form_partial.is_valid():
+            assert form_partial.cleaned_data["code"] is not None
+            instance = form_partial.save(commit=False)
+            instance.user = request.user
+            instance.save()
+            template_li = template + "#client-li-partial"
+            context = {'client':instance,'loading':True}
+            return render(request,template_li,context)
+        template_form = template + '#client-form-partial'
+        context_form = {'form':form_partial}
+        content = render(request,template_form,context_form)
+        content = reswap(content,"innerHTML")
+        return retarget(content,"#client-form-wrap")
+    context = {'clients':clients,'form':form,"switch":"False"}
+    return render(request,template,context)
+
+@login_required
+def hx_client_short_form(request):
+    if request.htmx:
+        form = ClientFormShort()
+        context = {'form':form}
+        template = 'session_client/lists/client_list.html#client-form-partial'
+        return render(request,template,context)
+    raise Http404("Not a expected request")
+
+
+
+
+@login_required
+def clients_hx_edit(request,client_pk):
+    """ manages clients htmx calls """
+    if request.method == 'PUT':
+        occurrence = get_object_or_404(ClientModel,pk=client_pk,user=request.user)
+        if occurrence.active:
+            occurrence.active = False
+        else:
+            occurrence.active = True
+        occurrence.archived_at = timezone.now()
+        occurrence.save()
+        return HttpResponse() # empty response that empties the li object
+    return HttpResponseClientRedirect(reverse("session_client:client_list"))
+
+@login_required
+def client_hx_item(request,client_pk):
+    if request.method == 'GET':
+        client = get_object_or_404(ClientModel,user=request.user,pk=client_pk)
+        template = 'session_client/item/client_modal.html'
+        context = {'client':client}
+        return render(request,template,context)
+    raise Http404("Not a expected request")
+
+@login_required
+def client_search_view(request):
+    template = 'session_client/lists/client_search.html'
+    if request.htmx:
+        form_partial = SearchClientForm(data=request.POST)
+        if form_partial.is_valid():
+            search_input = form_partial.cleaned_data['search_input']
+            active = form_partial.cleaned_data['active']
+            clients = ClientModel.objects.filter(code__icontains=search_input,
+                                                active=active,
+                                                )
+            template_calendar = template + "#client-list-partial"
+            context = {'clients':clients}
+            return render(request,template_calendar,context)
+        else:
+            raise Http404(f"The form has errors: {form_partial.errors}")
+    clients = ClientModel.objects.none() #? loaded by htmx after load
+    form = SearchClientForm()
+    context = {'clients':clients,'form':form}
+    return render(request,template,context)
+
+
+
+
+@login_required
+def client_archived_view(request):
+        template = 'session_client/lists/client_archived_list.html'
+        clients = ClientModel.objects.filter(user=request.user,active=False).order_by('code')
+        context = {'clients':clients}
+        return render(request,template,context)
+
+
+@login_required
+def add_client_view(request):
+    """add new client"""
+    if request.method !='POST':
+        #no data submitted; create a blank form
+        form = ClientForm()
+    else:
+        #POST data submitted; process data
+        form = ClientForm(data=request.POST)
+        if form.is_valid():
+            isinstance = form.save(commit=False)
+            isinstance.user = request.user
+            isinstance.save()
+            return redirect('session_client:client_list')
+    #display a blank or invalid form
+    context = {'form':form}
+    return render(request,'session_client/input/add_client.html',context)
+
+@login_required
+def edit_client_view(request,client_pk):
+    """edit an existing entry"""
+    client = get_object_or_404(ClientModel,pk=client_pk,user=request.user)
+    if request.method != 'POST':
+        #initial request;pre-fill form with the current entry
+        form = ClientForm(instance=client)
+    else:
+        #POST data submitted; process data
+        form = ClientForm(instance=client,data=request.POST)
+        if form.is_valid():
+            form.save()
+            messages.info(request,f"Client '{client.code}' updated")
+            return redirect('session_client:client_list')
+    context = {'client':client,'form':form}
+    return render(request,'session_client/edit/edit_client.html',context)
+
---
+++ session_client/views_session.py
@@ -0,0 +1,184 @@
+
+from django.shortcuts import render,redirect,get_object_or_404
+from django.urls import reverse
+
+from .models import ClientModel,SessionModel
+from django.contrib.auth.decorators import login_required
+from django.http import Http404,HttpResponse
+from .forms import SessionForm,SessionSelectGroupForm,SearchSessionFrom
+from django_htmx.http import retarget,HttpResponseClientRedirect
+import pendulum as p
+from django.contrib import messages
+
+
+# Create your views here.
+
+def index_view(request):
+    """show all session_client"""
+    return render(request,'session_client/index.html')
+
+
+#* session
+
+@login_required
+def sessions_view(request) -> HttpResponse:
+    """show open sessions, add a new simple session, update pay status with other hx-views
+    it filters sessions that are open and have already passed now() -1 hour as reference.
+    """
+    sessions = SessionModel.objects.filter(client__user=request.user, open=True).order_by('-date', '-start_time')
+    clients_user = ClientModel.objects.filter(user=request.user) or None # for select
+    template = 'session_client/lists/session_list.html'
+    form = SessionSelectGroupForm()
+    if request.htmx:
+        form_partial = SessionSelectGroupForm(data=request.POST)
+        if form_partial.is_valid():
+            only_unpaid = form_partial.cleaned_data['only_unpaid']
+            include_next = form_partial.cleaned_data['include_next']
+            sessions = SessionModel.objects.filter(client__user=request.user,open=True).order_by('-date','-start_time')
+            if only_unpaid:
+                sessions = sessions.filter(paid=False)
+            if not include_next:
+                sessions = sessions.filter(date__lte=p.now().date())
+            template_calendar = template + "#session-list-partial"
+            context = {'sessions':sessions}
+            return render(request,template_calendar,context)
+        # render form errors
+        template_partial = template + '#session-form-partial'
+        context = {'form':form_partial}
+        response = render(request,template_partial,context)
+        return retarget(response,"session-form-partial")
+    sessions = sessions.filter(date__lte=p.now().date())
+    form.fields['client'].queryset = clients_user
+    context = {'sessions':sessions,'form':form}
+    return render(request,template,context)
+
+@login_required
+def sessions_hx_edit_paid(request,session_pk):
+    """ manages clients htmx calls """
+    if request.method == 'PUT':
+        session = get_object_or_404(SessionModel,pk=session_pk,client__user=request.user)
+        if session.paid:
+            session.paid = False
+        else:
+            session.paid = True
+        session.save()
+        template = 'session_client/lists/session_list.html#session_paid'
+        context = {'session':session}
+        return render(request,template,context) # empty response that empties the li object
+    raise Http404("Not a expected request")
+
+@login_required
+def sessions_hx_edit_open(request,session_pk):
+    """ manages clients htmx calls """
+    if request.method == 'PUT':
+        session = get_object_or_404(SessionModel,pk=session_pk,client__user=request.user)
+        if session.open:
+            session.open = False
+        else:
+            session.open = True
+        session.save()
+        template = 'session_client/lists/session_list.html#session_open'
+        context = {'session':session}
+        return render(request,template,context) # empty response that empties the li object
+    raise Http404("Not a expected request")
+
+def session_hx_item(request,session_pk):
+    if request.method == 'GET':
+        session = get_object_or_404(SessionModel,client__user=request.user,pk=session_pk)
+        template = 'session_client/item/session_modal.html'
+        context = {'session':session}
+        return render(request,template,context)
+    raise Http404("Not a expected request")
+
+
+@login_required
+def sessions_search(request):
+    """ Search sessions by date and client """
+    template = "session_client/lists/session_search.html"
+    clients_user = ClientModel.objects.filter(user=request.user) or None # for select
+    if request.POST:
+        form_partial = SearchSessionFrom(data=request.POST)
+        if form_partial.is_valid():
+            start_ref = form_partial.cleaned_data['date_ref_start']
+            end_ref = form_partial.cleaned_data['date_ref_end']
+            client_ref = form_partial.cleaned_data['client'] or None
+            if client_ref:
+                # using session date as reference and csv generation.
+                sessions = SessionModel.objects.filter(date__gte=start_ref,
+                                                date__lte=end_ref,
+                                                client=client_ref).order_by('date','start_time')
+            else:
+                sessions = SessionModel.objects.filter(client__user=request.user,
+                                                date__gte=start_ref,
+                                                date__lte=end_ref,).order_by('date','start_time')
+            if request.htmx:
+                template_calendar = template + "#session-list-partial"
+                context = {'sessions':sessions}
+                return render(request,template_calendar,context)
+            import csv
+            start_ref_str = p.instance(start_ref).to_formatted_date_string() #converting for easier formatting
+            end_ref_str = p.instance(end_ref).to_formatted_date_string()
+            file_name = f"{client_ref}: {start_ref_str}-{end_ref_str}" if client_ref else f"{start_ref_str}-{end_ref_str}"
+            response = HttpResponse(
+            content_type="text/csv",
+            headers={"Content-Disposition": f'attachment; filename="{file_name}.csv"'})
+            fieldnames = ["date","start_time","client","brief","paid"]
+            writer = csv.writer(response) # response is the output
+            writer.writerow(fieldnames)
+            for row in sessions:
+                writer.writerow([row.date,row.start_time, row.client, row.brief, row.paid])
+            return response
+
+        else:
+            raise Http404(f"The form has errors: {form_partial.errors}")
+    sessions = SessionModel.objects.none() #? loaded by htmx after load
+    form = SearchSessionFrom()
+    form.fields['client'].queryset = clients_user
+    context = {'sessions':sessions,'form':form}
+    return render(request,template,context)
+
+
+@login_required
+def add_session_view(request):
+    """add new session"""
+    if request.method !='POST':
+        #no data submitted; create a blank form
+        form = SessionForm()
+    else:
+        #POST data submitted; process data
+        form = SessionForm(data=request.POST)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.user = request.user
+            instance.save()
+            return redirect('session_client:index')
+    #display a blank or invalid form
+    context = {'form':form}
+    return render(request,'session_client/input/add_session.html',context)
+
+@login_required
+def edit_session_view(request,session_pk):
+    """edit an existing Session"""
+    session = get_object_or_404(SessionModel,pk=session_pk,client__user=request.user)
+    if request.method != 'POST':
+        #initial request;pre-fill form with the current entry
+        form = SessionForm(instance=session)
+    else:
+        #POST data submitted; process data
+        form = SessionForm(instance=session,data=request.POST)
+        if form.is_valid():
+            form.save()
+            messages.info(request,f"Session '{session.brief}' updated")
+            return redirect("session_client:session_list")
+    context = {'session':session,'form':form}
+    return render(request,'session_client/edit/edit_session.html',context)
+
+@login_required
+def hx_delete_session(request,session_pk):
+    session = get_object_or_404(SessionModel, pk=session_pk,user=request.user)
+    if request.method == 'DELETE':
+        session.delete()
+        messages.info(request,f"Session '{session.start_time.strftime('%d-%m-%y,%H:%M')}' deleted")
+        return HttpResponseClientRedirect(reverse("session_client:session_list"))
+    messages.error(request,f"Session '{session.start_time.strftime('%d-%m-%y, %H:%M')}' not deleted")
+    return render(request,'_toasts.html')
