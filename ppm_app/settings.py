aa8f246f95d6ad63ad5a83838ef688d13db8ea06
GabeSoler <gsoler@gmail.com>
2025-12-03 13:52:33 +0000

    more url twicks +++++++

--- ppm_app/settings.py
+++ ppm_app/settings.py
@@ -158,7 +158,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "/var/"+ URL_BASE +"."+HOST_URL +"/static/"
+STATIC_URL = "https://"+ URL_BASE +"."+HOST_URL +"/static/"
 
 
 # Default primary key field type
