cf9068f537d8913dc4fa6c6c43220cd1d4f985fb
GabeSoler <gsoler@gmail.com>
2025-12-03 13:48:20 +0000

    more url twicks ++++

--- ppm_app/settings.py
+++ ppm_app/settings.py
@@ -160,6 +160,7 @@ USE_TZ = False
 
 STATIC_URL = "/var/"+ URL_BASE +"."+HOST_URL +"/static/"
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
