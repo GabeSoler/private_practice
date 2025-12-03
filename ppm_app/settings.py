67f36bc2d131ffc9573a6364992010262265b3fd
GabeSoler <gsoler@gmail.com>
2025-12-03 14:46:16 +0000

    title overflow fix

--- ppm_app/settings.py
+++ ppm_app/settings.py
@@ -158,7 +158,10 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "https://"+ URL_BASE +"."+HOST_URL +"/static/"
+if DEBUG:
+    STATIC_URL = "/static/"
+else:
+    STATIC_URL = "https://"+ URL_BASE +"."+HOST_URL +"/static/"
 
 
 # Default primary key field type
