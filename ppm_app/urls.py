e9e716e69cab2ea81f2704a953ef6f340e4c8aa0
GabeSoler <gsoler@gmail.com>
2025-12-03 14:15:34 +0000

    admin change

--- ppm_app/urls.py
+++ ppm_app/urls.py
@@ -3,10 +3,11 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from decouple import config
 
 urlpatterns = [
     path('admin/doc/', include('django.contrib.admindocs.urls')),
-    path("admin/", admin.site.urls),
+    path(config('ADMIN_URL'), admin.site.urls),
     path('accounts/',include('accounts.urls')),
     path('',include('session_client.urls')),
     path('calendar/',include('room_calendar_app.urls')),
