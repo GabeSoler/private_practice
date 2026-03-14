37843bf987545750917e5be6d6cbdafa850b3fb3
GabeSoler <gsoler@gmail.com>
2026-03-14 23:21:59 +0000

    updating staging

--- ai_chat/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class ChatForm(forms.Form):
-    chat_input = forms.CharField(label='Chat',
-                                 help_text="Ask about anything",
-                                 max_length=100)
-
--- my_numbers/apps 2.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MyNumbersConfig(AppConfig):
-    name = "my_numbers"
