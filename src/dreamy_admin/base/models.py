b23cc32e87c8e0cdddcaec5b0614e131da16cbf7
GabeSoler <gsoler@gmail.com>
2026-05-29 12:47:11 +0100

    str of base model

--- src/dreamy_admin/base/models.py
+++ src/dreamy_admin/base/models.py
@@ -9,10 +9,10 @@ class Page(models.Model):
     modified = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=200)
     content = models.TextField()
-    image = models.ImageField(upload_to='images/', blank=True)
+    image = models.ImageField(upload_to="images/", blank=True)
 
     def __str__(self):
-        return self.title
+        return f"{self.title}"
 
     def __repr__(self):
         return f"Base.Page:{self.title}"
@@ -27,12 +27,12 @@ class IndexPage(models.Model):
     # feature 1
     feature_title = models.CharField(max_length=200)
     feature_content = models.TextField()
-    feature_image = models.ImageField(upload_to='images/')
+    feature_image = models.ImageField(upload_to="images/")
     # features 2
     feature_title_2 = models.CharField(max_length=200)
     feature_content_2 = models.TextField()
-    feature_image_2 = models.ImageField(upload_to='images/')
+    feature_image_2 = models.ImageField(upload_to="images/")
     # features 3
     feature_title_3 = models.CharField(max_length=200)
     feature_content_3 = models.TextField()
-    feature_image_3 = models.ImageField(upload_to='images/')
+    feature_image_3 = models.ImageField(upload_to="images/")
